6cba0c2ba22f92b00c465f37483040bbcb35e523
renzhifan <zhifan6797@163.com>
2018-12-08 16:13:04 +0800

    数据表添加

--- app/Models/ThirdPartyCall.php
+++ app/Models/ThirdPartyCall.php
@@ -10,6 +10,7 @@ class ThirdPartyCall extends Model
     protected $table = 't_third_party_call';
     protected $primaryKey = 'oid';
     public $timestamps = false;
+    //UPDATE t_third_party_call SET third_party_call =ROUND(RAND()*(10-1)+1)
     public static function getThirdPartyCallNum()
     {
         return static::query()->selectRaw('typename,sum(third_party_call) as num')->whereNotNull('typename')->groupBy('typename')->get()->toArray();
